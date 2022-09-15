42a7eea3dcf1d538c6fc02c320cc6d69ec53dc1e
Gigabit101 <thegigabit101@gmail.com>
2022-09-15 14:45:56 +0100

    Backport latest 1.19 changes

--- common/src/main/java/net/creeperhost/ftbbackups/commands/BackupCommand.java
+++ common/src/main/java/net/creeperhost/ftbbackups/commands/BackupCommand.java
@@ -6,10 +6,12 @@ import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.suggestion.SuggestionProvider;
 import net.creeperhost.ftbbackups.BackupHandler;
 import net.creeperhost.ftbbackups.config.Config;
+import net.minecraft.client.server.IntegratedServer;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.SharedSuggestionProvider;
 import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.server.MinecraftServer;
 
 import java.util.Locale;
 
@@ -22,11 +24,17 @@ public class BackupCommand {
 
     public static LiteralArgumentBuilder<CommandSourceStack> register() {
         return Commands.literal("backup")
-                .requires(cs -> cs.hasPermission(Config.cached().command_permission_level))
+                .requires(cs -> cs.hasPermission(hasPerm(cs.getServer())))
                 .then(
                         Commands.argument("command", StringArgumentType.string()).suggests(SUGGESTIONS)
                                 .executes(cs -> execute(cs, StringArgumentType.getString(cs, "command")))
                 );
+    }
+
+    public static int hasPerm(MinecraftServer server)
+    {
+        if(server.isDedicatedServer() || server instanceof IntegratedServer integratedServer && integratedServer.isPublished()) return Config.cached().command_permission_level;
+        return 0;
     }
 
     private static int execute(CommandContext<CommandSourceStack> cs, String command) {
@@ -37,7 +45,6 @@ public class BackupCommand {
                 break;
         }
         if (Config.cached().manual_backups_time == 0) {
-            cs.getSource().getServer().saveAllChunks(true, true, true);
             BackupHandler.isDirty = true;
             BackupHandler.createBackup(cs.getSource().getServer(), isProtected);
         } else {
